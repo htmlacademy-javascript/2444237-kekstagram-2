8d967a92c49366edd23960ce828a44753d35841a
Vladimir <vlad.stavros@bk.ru>
2025-02-09 17:03:19 +0300

    Валидация формы

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { getArrayPhotos } from './create-array-photo.js';
 import {renderUserPhotos} from './render-photo.js';
 import {showBigPicture} from './show-big-photo.js';
+import './validateForm.js';
 
 const photos = getArrayPhotos();
 
